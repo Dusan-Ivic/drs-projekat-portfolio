464a1c6c2f21bfd6ad910adfeebf2eacef328d2e
Dusan-Ivic <ivicd99@gmail.com>
2022-12-17 18:48:51 +0100

    Connect server to database

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,26 @@
 from flask import Flask
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
+from os import getenv
+from sys import exit
+
+def connect_database(connection_string, database_name):
+  client = MongoClient(connection_string, serverSelectionTimeoutMS=1000)
+  try:
+    database = client.get_database(database_name)
+    print("MongoDB Connected")
+    return database
+  except PyMongoError as mongo_err:
+    print("MongoDB Connection Failed")
+    exit(mongo_err)
+
+db = connect_database(getenv("MONGO_URI"), getenv("DB_NAME"))
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+  db
   response_data = {
     "success": True,
     "message": "Welcome to Portfolio Project"
@@ -11,4 +28,4 @@ def index():
   return response_data, 200
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+  app.run(host="0.0.0.0")
