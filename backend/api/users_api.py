a7643d6aa26450dabb4dbde40c2507d309d6fcdb
Dušan Ivić <76745718+Dusan-Ivic@users.noreply.github.com>
2022-12-21 01:04:32 +0100

    Switch from pymongo to mongoengine (#9)
    * Switch to mongoengine for db
    
    * Fix missing validations

--- backend/api/users_api.py
+++ backend/api/users_api.py
@@ -1,116 +1,108 @@
-from flask import Blueprint, current_app, Response, request
+from flask import Blueprint, Response, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from bson.json_util import dumps
-from pymongo import ReturnDocument
-from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash
+from mongoengine import ValidationError, NotUniqueError, DoesNotExist
 from models.user import User
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 users_api = Blueprint("users_api", __name__)
 
 # Vrati sve korisnike
 @users_api.route("/api/users", methods=["GET"])
 def get_users():
-  with current_app.app_context():
-    users = list(current_app.db.users.find())
+  users = User.objects
 
-    # Konvertuje svaki id u string da bi obrisano $oid
-    for user in users:
-      user["_id"] = str(user["_id"])
+  # Konvertovanje u listu objekata
+  users = [user._data for user in users._iter_results()]
 
-    response_data = {
-      "success": True,
-      "count": len(users),
-      "data": users,
-    }
+  response_data = {
+    "success": True,
+    "count": len(users),
+    "data": users,
+  }
 
-    return Response(response=dumps(response_data), status=200, mimetype="application/json")
+  return Response(response=dumps(response_data), status=200, mimetype="application/json")
 
 # Dodaj novog korisnika
 @users_api.route("/api/users", methods=["POST"])
 def register_user():
-  with current_app.app_context():
-
-    user = User(**request.get_json()).serialize() 
-
-    #bez generate_password_hash-a
-    user["password"] = user["password"];
-    # user["password"] = generate_password_hash(user["password"])
-
-    current_app.db.users.insert_one(user)
-
-    # Konvertuje svaki id u string da bi obrisao $oid
-    user["_id"] = str(user["_id"])
-
+  user = User(**request.get_json())
+  
+  try:
+    user.save()
+  except (ValidationError, NotUniqueError) as err:
     response_data = {
-      "success": True,
-      "message": "User registered",
-      "data": user,
+      "success": False,
+      "message": "Validation failed",
+      "errors": err.to_dict(),
     }
+    return Response(response=dumps(response_data), status=400, mimetype="application/json")
 
-    return Response(response=dumps(response_data), status=201, mimetype="application/json")
+  response_data = {
+    "success": True,
+    "message": "User registered",
+    "data": user._data,
+  }
+
+  return Response(response=dumps(response_data), status=201, mimetype="application/json")
 
 # Izmeni postojeceg korisnika
 @users_api.route("/api/users/<id>", methods=["PUT"])
 @jwt_required()
 def update_user(id):
   auth_id = get_jwt_identity()
-  with current_app.app_context():
-    if auth_id != id:
-      return unauthorized() 
+  if auth_id != id:
+    return unauthorized() 
 
-    user_data = User(**request.get_json()).serialize()
-  
-    user = current_app.db.users.find_one_and_update(
-      { "_id": ObjectId(id) },
-      { "$set": {
-        "firstName": user_data["firstName"],
-        "lastName": user_data["lastName"],
-        "address": user_data["address"],
-        "city": user_data["city"],
-        "country": user_data["country"],
-        "phone": user_data["phone"],
-        "email": user_data["email"],
-        "password": user_data["password"],
-      }},
-      return_document=ReturnDocument.AFTER
-    )
+  try:
+    user = User.objects.get(id=id)
+  except DoesNotExist:
+    return not_found()
 
-    if user == None:
-      return not_found()
-
-    # Konvertuje svaki id u string da bi obrisano $oid
-    user["_id"] = str(user["_id"])
-
+  try:
+    User(**request.get_json()).validate()
+  except ValidationError as err:
     response_data = {
-      "success": True,
-      "message": "User edited",
-      "data": user
-     }
-    return Response(response=dumps(response_data), status=200, mimetype="application/json")
+      "success": False,
+      "message": "Validation failed",
+      "errors": err.to_dict(),
+    }
+    return Response(response=dumps(response_data), status=400, mimetype="application/json")
+
+  user.update(**request.get_json())
+  user.reload()
+
+  response_data = {
+    "success": True,
+    "message": "User edited",
+    "data": user._data
+  }
+
+  return Response(response=dumps(response_data), status=200, mimetype="application/json")
   
 # Obrisi korisnika
 @users_api.route("/api/users/<id>", methods=["DELETE"])
 @jwt_required()
 def delete_user(id):
   auth_id = get_jwt_identity()
-  with current_app.app_context():
-    if auth_id != id:
-      return unauthorized() 
+  if auth_id != id:
+    return unauthorized()
 
-    user = current_app.db.users.find_one_and_delete({ "_id": ObjectId(id) })
+  try:
+    user = User.objects.get(id=id)
+  except DoesNotExist:
+    return not_found()
 
-    if user == None:
-      return not_found()
+  user.delete()
 
-    response_data = {
-      "success": True,
-      "message": "User deleted",
-      "data": {
-        "_id": str(id)
-      },
+  response_data = {
+    "success": True,
+    "message": "User deleted",
+    "data": {
+      "id": str(user._data["id"])
     }
-    return Response(response=dumps(response_data), status=200, mimetype="application/json")
+  }
+
+  return Response(response=dumps(response_data), status=200, mimetype="application/json")
   
 @users_api.errorhandler(404)
 def not_found(error=None):
