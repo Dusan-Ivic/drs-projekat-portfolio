8d18b168b8e293ea0d4be55171db324875984373
Nikola <spacetoast369@gmail.com>
2023-01-20 17:06:04 +0100

    fix

--- frontend/src/pages/TransactionHistory.jsx
+++ frontend/src/pages/TransactionHistory.jsx
@@ -8,7 +8,6 @@ import {
 import Spinner from "../components/Spinner";
 import axios from "axios";
 
-
 const baseUrl = "http://localhost:5000";
 const token = localStorage.getItem("access_token");
 
@@ -25,7 +24,6 @@ const TransactionHistory = () => {
     },
   };
 
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -37,9 +35,6 @@ const TransactionHistory = () => {
       `http://localhost:5000/api/transactions/${id}`,
       config
     );
-
-    let a = axios.delete(`http://localhost:5000/api/transactions/${id}`);
-
   };
 
   useEffect(() => {
